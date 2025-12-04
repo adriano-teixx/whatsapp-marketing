5d3d210e1f247fd192e3b307e779b9d977b1f4b2
adrianoteixx <adriano.teixxx@gmail.co>
2025-12-04 15:17:34 -0300

    Initial import

---
+++ public/build/assets/useRtl-8a78f518.js
@@ -0,0 +1,1 @@
+import{K as e,m as o}from"./app-b9b41bfa.js";function i(){const r=e(),t=o(()=>r.props.isRtl),s=o(()=>({"direction-rtl":t.value,"direction-ltr":!t.value}));return{isRtl:t,rtlClass:s}}export{i as u};
